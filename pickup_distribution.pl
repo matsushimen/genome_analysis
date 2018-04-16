e9fab797dfaf82d5a267becb8b664594ee2be430
matsushimen <m172240@hiroshima-u.ac.jp>
2018-04-16 15:21:49 +0900

    dev

--- pickup_distribution.pl
+++ pickup_distribution.pl
@@ -1,96 +1,34 @@
-###dummy###
-use lib qw(./Mod);
-use Mod::Posdata;
-use Mod::Scorefile;
-use Statistics::Lite qw(:all);
-
-$stream= shift @ARGV;
-$savefile = shift @ARGV;
-@datafiles = @ARGV;
-
-$count = 0;
-
-@scorefiles = grep { /\.data$/ } @datafiles;
-@posfiles = grep { /\.txt$/ } @datafiles;
-open OUT ,"> $savefile"or die;
-foreach my $posfile (@posfiles){
-    my $chr;
-    
-    ###スコアファイル(wigファイル)を開く###
-    
-    if ($posfile =~ /(chr[0-9]+)/g){
-        $chr = $1;
-    }
-    else{
-        "Chromosome type couldn't be found. Input the chromosome number :";
-        $chr = "chr".<STDIN>;
-        chomp($chr);
-    }
-    print $chr,"\n";
-    my @tmp = grep { $_=~ /$chr\.data/ } @scorefiles;
-    my $scorefile = $tmp[0];
-    print "Opening $posfile ...\n";
-    print "Searching in $scorefile\n";
-    open SCORE, "$scorefile"or die;
-    my $data = undef;
-    $data = Mod::Scorefile -> new();
-    while (my $line = <SCORE>){
-        chomp $line;
-        $data -> new_data($line);
-    }
-    close SCORE;
-    print "Score set has made.\n";
-    ##################################
-    open POS, "$posfile"or die;
-    while (my $line = <POS>){
-        chomp $line;
-        my $pos = Mod::Posdata->new();
-        $pos = $pos -> set_position_data($line);
-        my $start = $pos -> start();
-        my $end = $pos -> end();
-        my $score = $pos -> score();
-        #print "$chr $start $end\n";
-        my @up = ();
-        my @down = ();
-        my @seq = ();
-        for(my $i = $start - $stream +75; $i <$start +75; $i++){
-            my $scr = $data -> get_score_from_position($i);
-            if($scr != -1){
-                push @up,$scr;
+my @data;
+while(my $line = <BED>){
+    my @bed = split " ",$line;
+    my $bs = $bed[1];
+    my $be = $bed[2];
+    my $us = $bs - $st;
+    my $ue = $bs;
+    my $ds = $be;
+    my $de = $be + $st;
+    my $u_flug = 0;
+    while(my $wig = <WIG>){
+        if($wig =~ /fixed/){
+            
+            my @wig = split $wig;
+            my $ws = $wig[1];
+            my $we = $wig[2];
+            if(($u_flug==0)&&($ws<=$ue)){
+                
             }
-            else{
-                push @up,-1;
+            if(($ws<=$us)&&($us<=$we)){
+                if(($ws<=$ue)&&($ue<=$we)){
+                    for(my $i =0; $i<$st, $i++){#listにぶちこみ
+                        push @{$data->[$i]},$wig[3];
+                    }
+                }
+                else{
+                    for(my $i =0; $i<$$we-$us, $i++){#listにぶちこみ
+                        push @{$data->[$i]},$wig[3];
+                    }
+                }
             }
         }
-        
-        for(my $i = $end + 1 -75; $i <= $end + $stream -75; $i++){
-            my $scr = $data -> get_score_from_position($i);
-            if($scr != -1){
-                push @down,$scr;
-            }
-            else{
-                push @down,-1;
-            }
-        }
-        for(my $i = $start; $i <= $end; $i++){
-            my $scr = $data -> get_score_from_position($i);
-            if($scr != -1){
-                push @seq,$scr;
-            }
-            else{
-                push @seq,0;
-            }
-        }
-        my $cn = int(($end-$start)/2);
-        my $mini = min(@seq);
-        my $cent = $seq[$cn];
-        my $mn = mean(@seq);
-        print OUT "$chr $start $end $score $mini $cent $mn (@up/@down)\n";
-        $count++;
     }
-    close POS;
-    $data = undef;
 }
-close OUT;
-print "Number = $count\n";
-
