36fe7c737098f8a179475407a90b2abfb35e906e
松島　佑樹 <matsuhima@MatsushiMac.local>
2018-04-19 14:56:44 +0900

    commit

--- pickup_distribution.pl
+++ pickup_distribution.pl
@@ -1,4 +1,21 @@
 my @data;
+sub get_scores{
+    (my $bs, my $be, my $ws, my $we, my $score) = @_;
+    my @list;
+    
+    if(($ws<=$bs)&&($bs<=$we)){
+        if(($ws<=$be)&&($be<=$we)){#読み込んだwig行の中に指定領域が含まれている場合
+            for(my $i =0; $i<$st, $i++){#listにぶちこみ
+                push @{$data->[$i]},$wig[3];
+            }
+        }
+        else{
+            for(my $i =0; $i<$$we-$us, $i++){#listにぶちこみ
+                push @{$data->[$i]},$wig[3];
+            }
+        }
+    }
+}
 while(my $line = <BED>){
     my @bed = split " ",$line;
     my $bs = $bed[1];
