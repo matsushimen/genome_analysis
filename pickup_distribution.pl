9a7e1b7136602861e2b4e6299154438628681a37
matsushiman <m172240@hiroshima-u.ac.jp>
2018-06-15 15:33:52 +0900

    version 1.01

--- pickup_distribution.pl
+++ pickup_distribution.pl
@@ -91,8 +91,7 @@ while(my $line_pos = <POS>){
             }
             if($WS > $left_last){#ギャップの穴埋め
                 for(my $i = 0;$i < $WS - $left_last;$i++){
-                    print LOG "-1.0 ";
-                    exit(1);
+                    print LOG "* ";
                     $left_pos++;
                 }
                 $left_last = $WS;#スタート位置の移動
@@ -116,7 +115,7 @@ while(my $line_pos = <POS>){
             }
             if($WS > $right_last){#前回位置から現在位置までのギャップの穴埋め
                 for(my $i = 0;$i < $WS - $right_last;$i++){
-                    print LOG "-1.0 ";
+                    print LOG "* ";
                     $right_pos++;
                 }
                 $right_last = $WS#スタート位置の移動
