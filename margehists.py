294fa67ca505b7200fd5c2b6cfa02178ce78a2f0
matsushiman <m172240@hiroshima-u.ac.jp>
2018-09-08 13:05:24 +0900

    new

--- margehists.py
+++ margehists.py
@@ -21,14 +21,13 @@ for hist in hists:
         
         if(open_f == 0):
             labels.append(data[0])
-            print (data[0])
             means[data[0]] = float(data[1])*int(data[3])
-            meansquare[data[0]] = (float(data[2])+means[data[0]]*means[data[0]])*int(data[3])
+            meansquare[data[0]] = (float(data[2])+float(data[1])*float(data[1]))*int(data[3])
             number[data[0]] = int(data[3])
             
         else:
             means[data[0]] += float(data[1])*int(data[3])
-            meansquare[data[0]] += (float(data[2])+means[data[0]]*means[data[0]])*int(data[3])
+            meansquare[data[0]] += (float(data[2])+float(data[1])*float(data[1]))*int(data[3])
             number[data[0]] += int(data[3])
 
     open_f = 1
@@ -41,13 +40,19 @@ number_values = []
 #append to lists
 for name in labels:
     mean_values.append(means[name]/number[name])
-    variance_values.append((meansquare[name]-means[name]*means[name])/(number[name]-1))
+    variance_values.append((meansquare[name]/number[name]-(means[name]/number[name])*(means[name]/number[name])))
     number_values.append(number[name])
 
 mean_values = np.array(mean_values)
 variance_values = np.array(variance_values)
 number_values = np.array(number_values)
 
+with open(savename+".txt",mode='write') as f:
+    for n,i,j,k in zip(labels,mean_values,variance_values,number_values):
+        string = str(n)+" "+str(i)+" "+str(j)+" "+str(k)+"\n"
+        f.write(string)
+
+
 plt.figure()
-plt.bar(range(len(labels)), mean_values)
-plt.savefig(savename,transparent=True,yerr=np.sqrt(variance_values/number_values))
+plt.bar(range(len(labels)), mean_values, yerr=np.sqrt(variance_values/number_values), ecolor="red")
+plt.savefig(savename,transparent=True)
